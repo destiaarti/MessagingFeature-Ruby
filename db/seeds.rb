c824e21ce01ef0e786d6f18b98140fe8c0180c17
destiaarti <destiaartiw@gmail.com>
2021-01-31 07:44:15 +0700

    seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+#seeder user
+User.create(name:'userA', password:'toy', email:"userA@mail.com")
+User.create(name:'userB', password:'toy', email:"userB@mail.com")
+User.create(name:'userC', password:'toy', email:"userC@mail.com")
+User.create(name:'userD', password:'toy', email:"userD@mail.com")
+
+#seeder Conversation
+Conversation.create(senderid:1, receiver_id:2)
+Conversation.create(senderid:1, receiver_id:4)
+Conversation.create(senderid:3, receiver_id:1)
+Conversation.create(senderid:3, receiver_id:2)
+Conversation.create(senderid:3, receiver_id:4)
+
+#seeder A-B
+Message.create(body:'hi', conversation_id:1, read:1, user_id:1)
+Message.create(body:'hi too', conversation_id:1, read:1, user_id:2)
+Message.create(body:'whats up?', conversation_id:1, read:1, user_id:2)
+Message.create(body:'good', conversation_id:1, read:0, user_id:1)
+Message.create(body:'if you?', conversation_id:1, read:0, user_id:1)
+
+#seeder A-C
+Message.create(body:'yoloo', conversation_id:2, read:1, user_id:1)
+Message.create(body:'yeyyy', conversation_id:2, read:1, user_id:4)
+
+#seeder A-D
+Message.create(body:'morning', conversation_id:3, read:0, user_id:3)
+Message.create(body:'.....', conversation_id:3, read:0, user_id:3)
+Message.create(body:'.....', conversation_id:3, read:0, user_id:3)
